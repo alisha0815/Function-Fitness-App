87cd829346c3011ed10624aa473b97b1d85448fd
Matthaios Stioukis <stioukimattigr@gmail.com>
2021-05-22 14:53:43 +0300

    Trying to implement reviews, and my code is not up to date

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,5 +2,6 @@ class DashboardsController < ApplicationController
   def index
     @personalized_trainers = current_user.personalized_trainers
     @personalized_trainers = PersonalizedTrainer.order(params[:sort])
+    @bookings = Booking.where(user: current_user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   end
 
   get 'dashboard', to: 'dashboards#index'
+  resources :bookings, only: [] do 
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
