df68cfb71c1fb6cda739f31d662f2e123057e8db
jungah sohn <aliciasohn815@gmail.com>
2021-05-22 12:57:01 +0200

    fixed booking form

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,11 +7,15 @@ class Booking < ApplicationRecord
   validate :end_date_after_start_date
   # validates_length_of :goal, minimum: 6
 
+  def booked_date_range
+    { from: :start_date, to: :end_date }
+  end
+
   private
 
   def end_date_after_start_date
     return if end_date.blank? || start_date.blank?
-
+    
     if end_date < start_date
       errors.add(:end_date, "must be after the start date")
     end
